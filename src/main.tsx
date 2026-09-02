a378a0e049f708dd2c25435a5244a5a431f5dcf4
machxanht <machxanht@gmail.com>
2026-09-03 02:52:29 +0700

    ui: render account and project identity banner

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {IdentityBanner} from './components/IdentityBanner.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <IdentityBanner />
     <App />
   </StrictMode>,
 );
