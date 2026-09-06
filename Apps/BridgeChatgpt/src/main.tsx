86ae51664ace924ce73eb683745156136f78d107
machxanht <machxanht@gmail.com>
2026-09-06 15:13:20 +0700

    feat(ui): mount project setup status control

--- Apps/BridgeChatgpt/src/main.tsx
+++ Apps/BridgeChatgpt/src/main.tsx
@@ -1,10 +1,12 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import { ProjectSetupStatusBar } from './components/ProjectSetupStatusBar.js';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ProjectSetupStatusBar />
   </StrictMode>,
 );
